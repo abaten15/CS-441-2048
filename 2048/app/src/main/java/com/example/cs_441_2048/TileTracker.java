752f8fcce53994a6a8a917afaa00a0bb6ebea5f7
Nick Abate <abaten15@nicks-mbp-4.home>
2019-02-05 18:44:45 -0500

    added tile layout and TileTracker class

---
+++ 2048/app/src/main/java/com/example/cs_441_2048/TileTracker.java
@@ -0,0 +1,21 @@
+package com.example.cs_441_2048;
+
+import android.widget.ImageView;
+
+public class TileTracker {
+
+    int value = 0;
+    private int ID;
+
+    public TileTracker(int ID) {
+
+        this.ID = ID;
+
+    }
+
+    public void update(MainActivity mainActivity) {
+        ImageView imageView = mainActivity.findViewById(ID);
+        imageView.
+    }
+
+}
